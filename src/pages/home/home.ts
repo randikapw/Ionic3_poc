0549007c90f5de2cab6086dc96d9ffc873f3b34a
Randika <randikapw@gmail.com>
2018-02-15 05:55:15 +0530

    Refresher and Toast
    Implemented Simple refresher and Toase in Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController,ToastController } from 'ionic-angular';
 
 import { MyModalComponent } from '../../components/my-modal/my-modal'
 
@@ -10,13 +10,41 @@ import { MyModalComponent } from '../../components/my-modal/my-modal'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,public modalCtrl: ModalController) {
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController,
+    private toastCtrl: ToastController
+  ) {
 
   }
 
   onBtnClick(){
     let modal = this.modalCtrl.create(MyModalComponent, 0);
     modal.present();
+  }
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+      this.presentToast();
+    }, 2000);
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'User was added successfully',
+      duration: 3000,
+      position: 'top'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
 }
